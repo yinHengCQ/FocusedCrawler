40452d495fb3296e5d1ba6da8df265625a32514f
yinHeng <yinhengcq@163.com>
2018-08-20 19:24:38 +0800

    Update tornado_server.py

--- tornado_server.py
+++ tornado_server.py
@@ -1,75 +1,92 @@
-# -*- coding: utf-8 -*-
-
-import time, os
+import tornado.ioloop
+import tornado.web
+import os,cv2,json
+import numpy as np
 import tensorflow as tf
-import origin
-from config import ps_hosts, worker_hosts
-from socket_client import send_finish_info, local_ip
-
-GPU_USE_RATE = 0.9
-
-# 模型保存的路径。
-MODEL_SAVE_PATH = "./model"
-
-FLAGS = tf.app.flags.FLAGS
-
-tf.app.flags.DEFINE_string('job_name', 'worker', ' "ps" or "worker" ')
-tf.app.flags.DEFINE_integer('task_index', 0, 'Task ID of the worker/replica running the training.')
-tf.app.flags.DEFINE_string("cuda", "", "specify gpu")
-
-if FLAGS.cuda:
-    os.environ["CUDA_VISIBLE_DEVICES"] = FLAGS.cuda
-
-is_chief = (FLAGS.task_index == 0)
-
-start_time = time.time()
+from PIL import Image
 
 
-class MyStopAtStepHook(tf.train.StopAtStepHook):
-    def after_run(self, run_context, run_values):
-        global_step = run_values.results + 1
-        if global_step >= self._last_step:
-            print("task finish,total time count:%s" % (time.time() - start_time))
-            send_finish_info()
-            step = run_context.session.run(self._global_step_tensor)
-            if step >= self._last_step:
-                run_context.request_stop()
+
+saver = tf.train.import_meta_graph("./model/model.ckpt-2605.meta", clear_devices=True)
+graph = tf.get_default_graph()
+x = graph.get_tensor_by_name("data-input:0")
+pred = graph.get_tensor_by_name("predict_max_idx:0")
+keep = graph.get_tensor_by_name("keep-prob:0")
+
+def resize_img(img_file):
+    img = Image.open(img_file)
+    if img.size[0]==img.size[1]==64:
+        return img_file
+    else:
+        img = img.resize((64, 64), Image.ANTIALIAS)
+        # new_img="./temp/temp.png"
+        img.save(img_file)
+        return img_file
+
+def get_result(img_file):
+    with tf.Session() as sess:
+        saver.restore(sess, tf.train.latest_checkpoint("./model/"))
+        predict = sess.run(pred, feed_dict={x: [np.asarray(cv2.imread(resize_img(img_file)), dtype=np.float32).flatten()], keep: 1.0})
+        result=predict[0][0]+1
+        if result==1:
+            return "anger"
+        elif result==2:
+            return "contempt"
+        elif result == 3:
+            return "disgust"
+        elif result == 4:
+            return "fear"
+        elif result == 5:
+            return "happy"
+        elif result == 6:
+            return "sadness"
+        elif result == 7:
+            return "surprise"
+        else:
+            return "i don't know"
+
+class UploadFileHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('''
+        <html>
+          <head><title>Upload File</title></head>
+          <body>
+            <form action='file' enctype="multipart/form-data" method='post'>
+            <input type='file' name='file'/><br/>
+            <input type='submit' value='submit'/>
+            </form>
+          </body>
+        </html>
+        ''')
+
+    def post(self):
+        #文件的暂存路径
+        ret = {'result': 'OK'}
+        upload_path=os.path.join(os.path.dirname(__file__),'files')
+        #提取表单中‘name’为‘file’的文件元数据
+        file_metas=self.request.files['file']
+        for meta in file_metas:
+            filename=meta['filename']
+            filepath=os.path.join(upload_path,filename)
+            #有些文件需要已二进制的形式存储，实际中可以更改
+            with open(filepath,'wb') as up:
+                up.write(meta['body'])
+            out=get_result("./files/"+filename)
+            ret['result']=out
+            self.write(json.dumps(ret))
+            os.remove("./files/"+filename)
+
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*") # 这个地方可以写域名
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
 
 
-def main(argv=None):
-    cluster = tf.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
-    server = tf.train.Server(cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
-
-    if FLAGS.job_name == 'ps':
-        with tf.device("/cpu:0"):
-            server.join()
-
-    hooks = [MyStopAtStepHook(last_step=origin.TRAINING_STEPS)]
-    device_setter = tf.train.replica_device_setter(ps_device="/job:ps/task:%d" % FLAGS.task_index,
-                                                   worker_device="/job:worker/task:%d" % FLAGS.task_index,
-                                                   cluster=cluster)
-
-    with tf.device(device_setter):
-        origin.build_model(is_chief)
-        sess_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False,
-                                     gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=GPU_USE_RATE))
-
-    with tf.train.MonitoredTrainingSession(master=server.target, is_chief=is_chief, checkpoint_dir=MODEL_SAVE_PATH,
-                                           hooks=hooks,
-                                           save_checkpoint_secs=origin.SAVE_CHECKPOINT_SECS,
-                                           config=sess_config) as mon_sess:
-        print("session started.")
-        step = 0
-
-        while True:
-            current_info = origin.train_model(session=mon_sess, step=step)
-            for info in current_info:
-                print(info)
-            step += 1
+app=tornado.web.Application([
+    (r'/file',UploadFileHandler),
+])
 
 
-if __name__ == "__main__":
-    try:
-        tf.app.run()
-    except:
-        exit(1)
+if __name__ == '__main__':
+    app.listen(3000)
+    tornado.ioloop.IOLoop.instance().start()
